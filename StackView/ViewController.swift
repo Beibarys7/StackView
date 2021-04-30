77dc7a3dabd2a9dfba170fe7593d90a20fb86cb3
Beibarys <beibarys1927@gmail.com>
2021-04-30 15:43:27 +0600

    Initial commit

---
+++ StackView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackView
+//
+//  Created by Бейбарыс Шагай on 3/15/21.
+//  Copyright © 2021 Бейбарыс Шагай. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
